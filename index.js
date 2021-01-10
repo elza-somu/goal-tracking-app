af0c32a65748b5e4de8556b9a16c8944e09b33d5
Bramarika <bramarika.maddali@gmail.com>
2021-01-10 11:59:41 -0500

    Adding basic structure to the backend/database

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 const app = express();
 const port = 3000;
 const path = require('path');
 
+// All middlewares
 // Public Folders route
 app.use(express.static(path.join(__dirname, 'public')));
+// To log server req/response
+app.use(morgan('tiny'));
+// parse request using body-parser to read the params from request
+app.use(bodyparser.urlencoded({extended:true}));
+
 
 // Load view engine
-app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname,'views'));
+
 
 app.get('/', (req, res) => {
-  res.send('index', {});
+  res.render('index', {});
 }); 
 app.get('/add', (req, res) => {
   res.render('add', {});
