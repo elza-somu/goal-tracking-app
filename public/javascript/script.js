9dc65cd246f7e693dd6a55d4d8ee7404b8ea19a8
elza-s <elza_somu@yahoo.com>
2021-01-11 13:01:46 -0500

    added links to the navbar and route on click

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,11 @@
-alert("hi");
+let form = document.querySelector(".form");
+let title = document.querySelector("#inputName");
+let addGoal = document.querySelector(".add-item");
+
+function displayTitle(event){
+  event.preventDefault();
+
+  console.log(title)
+}
+
+addGoal.addEventListener('submit', displayTitle)
