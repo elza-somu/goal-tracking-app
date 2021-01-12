b6da806fa7019f327c6846f44e1d0ef9df460e5a
Bramarika <bramarika.maddali@gmail.com>
2021-01-12 06:17:27 -0500

    Made changes to retrieve/update/delete single task, work in progress

--- backend/services/render.js
+++ backend/services/render.js
@@ -1,5 +1,6 @@
 const {Task} = require('../model/tasks');
 
+//Index page display with all the tasks
 exports.homeRoute = (req,res) =>{
     Task.find({}, function(err, tasks){
         if(err){
@@ -12,6 +13,7 @@ exports.homeRoute = (req,res) =>{
     });
 }
 
+//Render Add items page
 exports.addItem = (req, res) => {
     res.render('add');
 }
@@ -19,25 +21,81 @@ exports.addItem = (req, res) => {
 // Adding post request from Add Item page
 exports.add = (req,res)=>{
     let task = new Task();
-    task.title = req.body.title;
-    task.description = req.body.description;
-    task.start_date = req.body.start_date;
-    task.end_date = req.body.end_date;
+        task.title = req.body.title;
+        task.description = req.body.description;
+        task.start_date = req.body.start_date;
+        task.end_date = req.body.end_date;
 
-    task.save(function(err){
-        if(err){
-            console.log(err);
-            return;
-        }else{
-        res.redirect('/')
+        //Have to make this a seperate function to calculate hours_spend
+        console.log(end_date);
+        var seconds = Math.floor((Date - (start_date))/1000);
+        var minutes = Math.floor(seconds/60);
+        var hours = Math.floor(minutes/60);
+        var days = Math.floor(hours/24);
+        task.hours_spend = hours-(days*24);
+        console.log(task.hours_spend);
+        //create seperate function end
+        task.save(function(err){
+            if(err){
+                console.log(err);
+                return;
+            }else{
+            res.redirect('/')
+        }
+        })
+        
+}
+
+//To View the Single Item
+exports.viewItem = (req, res) =>{
+    if(req.query.id){
+        const id = req.query.id;
+        Task.findById(id)
+            .then(data => {
+                if(!data){
+                    res.status(404).send({message:"Task not found"})
+                }else{
+                    res.render('view',{task:task})
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({message: "Couldn't retrieve Task"});
+            })
     }
-    })
-    
-}
-exports.trackItem = (req, res) => {
-    res.render('track');
 }
 
+//Track Item page for the task id
+exports.trackItem = (req, res) => {
+    if(!req.body){
+        return res
+        .status(400).send({message:"Data is empty"})
+    }
+    const id = req.params.id;
+    Task.findByIdAndUpdate(id, req.body,{useFindAndModify:false})
+        .then(data =>{
+            if(!data){
+                res.status(404).send({message:'Task not found'});
+            }else{
+                res.render('track',{data});
+            }
+        })
+        .catch(err => {
+            res.status(500).send({message:"Error updating task information"});
+        });
+}
+
+//Delete item based on task id
 exports.deleteItem = (req, res) => {
-    res.render('delete');
+    const id = req.params.id;
+    Task.findByIdAndDelete(id)
+    .then(data=>{
+        if(!data){
+            res.status(404).send({message:'Cannot delete task'});
+        }else{
+            res.render('delete');
+        }
+    })
+    .catch(err =>{
+        res.status(500).send({message:'Error deleting task information'});
+    })
 }
