6e5b78cd7b090cf1d8ec5c97e22c641f1acf24f7
elza-s <elza_somu@yahoo.com>
2021-01-12 19:04:53 -0500

    added validation for add page

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,4 +1,20 @@
-let form = document.querySelector(".form");
-let title = document.querySelector("#inputName");
-let addGoal = document.querySelector(".add-item");
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
